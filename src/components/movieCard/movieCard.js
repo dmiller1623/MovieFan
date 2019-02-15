a56d4ed3270e061777b0ebacedd98244c866c923
Dennis <dmiller1623@gmail.com>
2019-02-14 18:15:05 -0700

    Add Movie card component

---
+++ src/components/movieCard/movieCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class MovieCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default MovieCard
--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -1,6 +1,9 @@
+import React from 'react'
+mport MovieCard from '../movieCard'
+
 const MovieList = (movies) => {
   const displayMovies = movies.map((movie, index) => {
-    
+
   })
 
   return (
