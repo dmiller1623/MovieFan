591d7cf971c6440583ef1732e1e41676c045a525
Dennis <dmiller1623@gmail.com>
2019-02-14 17:41:14 -0700

    Add apicall for getting pop movies

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getPopularMovies } from '../../utilities/apiCalls/apiCalls'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount = () => {
+    this.getMovies()
+  }
+
+  getMovies = async () => {
+    await getPopularMovies()
+  }
+
+  
   render() {
     return (
       <div className="App">
